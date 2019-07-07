8512c9d7fb138ce868ec17c3aaab715a90cc23be
Estuardo López <estuardolh@gmail.com>
2019-07-07 15:13:20 -0600

    static list & raw button

--- app/src/main/java/com/android/papyrus502/papyrus/MainActivity.java
+++ app/src/main/java/com/android/papyrus502/papyrus/MainActivity.java
@@ -1,37 +1,73 @@
 package com.android.papyrus502.papyrus;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String words[] = new String[]{"sample1","sample2"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        Button newWordButton = (Button)findViewById(R.id.add_new_word);
+        //newWordEditText.setEnabled(false);
+        //newWordButton.setTypeface(Typeface.DEFAULT_BOLD);
+        newWordButton.setText("    Add a new word...");
+        newWordButton.setAllCaps(false);
+        newWordButton.setGravity(Gravity.START);
+        newWordButton.setTextColor(Color.GRAY);
+        newWordButton.setBackgroundColor(Color.WHITE);
+        newWordButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT).show();
+            }
+        });
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+
+        /*
+        * Add list
+        * */
+        ListView listView = (ListView) findViewById(R.id.list_view);
+
+        final ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, words);
+
+        listView.setAdapter(listViewAdapter);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
